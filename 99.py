48bc771b891876cca163c5d855013b6963c4a5f2
kabir <jxwazxzhp@126.com>
2015-04-29 14:51:23 +0800

    modified

--- 99.py
+++ 99.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3.3
-for a in range(1,10):
-	for b in range(1,10) :
+basevalue=15
+for a in range(1,basevalue):
+	for b in range(1,basevalue) :
 		if a>=b :
-			print(repr(b).ljust(1)+"x"+repr(a).ljust(1)+"="+repr(b*a).ljust(2),end=' ')
+			print(repr(b).ljust(1)+"x"+repr(a).ljust(1)+"="+repr(b*a).ljust(3),end=' ')
 	print('')
